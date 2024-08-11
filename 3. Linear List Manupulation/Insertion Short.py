93e66af3005ea481cbc875b43002bf98ff9543d9
Vibhanshu <vibhanshu28july@gmail.com>
2024-08-11 11:57:27 +0530

    Add files via upload

---
+++ 3. Linear List Manupulation/Insertion Short.py
@@ -0,0 +1,15 @@
+def insertionshort(list):
+    l=len(list)
+    for i in range (1,l):
+        t=list[i]
+        p=i-1
+        while(t<list[p] and p>=0):
+            list[p+1]=list[p]
+            p-=1
+        list [p+1]=t
+        print("After Pass No.",i,"",list)
+
+list=[24,22,-6,34,80,-43]
+insertionshort(list)
+print(list)
+
---
+++ 3. Linear List Manupulation/Linear Search.py
@@ -0,0 +1,14 @@
+def lsearch(lst,val):
+    f=0
+    l=len(lst)
+    for i in range (0,l):
+        if val==lst[i]:
+            print("Value Found At",(i+1),"position")
+            f=1
+            break
+        if (f==0):
+            print("Value Not Found")
+
+l=[34,56,98,25,4,5,66,12,79,6,3,45,2,98,10]
+v=int(input("Enter The Value To Search "))
+lsearch(l,v)
